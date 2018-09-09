63dfe715bab0f5813f2f1a9e7891ef6a5e41d22b
Jordan Mavrogeorge <jordomav@gmail.com>
2018-09-09 14:37:30 -0700

    show in DOM

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function add() {
+    var answer = document.getElementById("answer");
     var one = parseInt(document.getElementById("one").value);
     var two = parseInt(document.getElementById("two").value);
     var three = parseInt(document.getElementById("three").value);
     var four = parseInt(document.getElementById("four").value);
     var five = parseInt(document.getElementById("five").value);
     var added = one + two + three + four + five;
-    console.log(added);
+    answer.textContent = added;
 }
 
 document.getElementById("myBtn").addEventListener("click", add);
